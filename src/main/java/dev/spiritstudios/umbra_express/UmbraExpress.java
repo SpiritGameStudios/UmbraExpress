3c059a3b7ac9f922e46a84e367fdeb2d81b3a118
SkyNotTheLimit <159592458+ekulxam@users.noreply.github.com>
2025-11-30 18:39:54 -0800

    Feat: spectator on joining active game

--- src/main/java/dev/spiritstudios/umbra_express/UmbraExpress.java
+++ src/main/java/dev/spiritstudios/umbra_express/UmbraExpress.java
@@ -4,6 +4,9 @@ import dev.doctor4t.trainmurdermystery.block.NeonPillarBlock;
 import dev.doctor4t.trainmurdermystery.block.NeonTubeBlock;
 import dev.doctor4t.trainmurdermystery.block.OrnamentBlock;
 import dev.doctor4t.trainmurdermystery.block.ToggleableFacingLightBlock;
+import dev.doctor4t.trainmurdermystery.cca.AreasWorldComponent;
+import dev.doctor4t.trainmurdermystery.cca.GameWorldComponent;
+import dev.doctor4t.trainmurdermystery.compat.TrainVoicePlugin;
 import dev.spiritstudios.umbra_express.init.UmbraExpressCommands;
 import dev.spiritstudios.umbra_express.init.UmbraExpressBlocks;
 import dev.spiritstudios.umbra_express.init.UmbraExpressConfig;
@@ -14,9 +17,12 @@ import dev.spiritstudios.umbra_express.init.UmbraExpressBlockEntities;
 import eu.midnightdust.lib.config.MidnightConfig;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.block.Block;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Identifier;
+import net.minecraft.world.GameMode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -37,6 +43,20 @@ public class UmbraExpress implements ModInitializer {
 		UmbraExpressBlockEntities.init();
 
 		CommandRegistrationCallback.EVENT.register(UmbraExpressCommands::init);
+
+		ServerPlayerEvents.JOIN.register(player -> {
+			ServerWorld serverWorld = player.getServerWorld();
+			GameWorldComponent.GameStatus gameStatus = GameWorldComponent.KEY.get(player.getWorld()).getGameStatus();
+			if (gameStatus != GameWorldComponent.GameStatus.ACTIVE && gameStatus != GameWorldComponent.GameStatus.STARTING) {
+				return;
+			}
+
+			player.changeGameMode(GameMode.SPECTATOR);
+			TrainVoicePlugin.addPlayer(player.getUuid());
+
+			AreasWorldComponent.PosWithOrientation spectatorSpawnPos = AreasWorldComponent.KEY.get(serverWorld).getSpectatorSpawnPos();
+			player.teleport(serverWorld, spectatorSpawnPos.pos.getX(), spectatorSpawnPos.pos.getY(), spectatorSpawnPos.pos.getZ(), spectatorSpawnPos.yaw, spectatorSpawnPos.pitch);
+		});
     }
 
 	public static Identifier id(String path) {
