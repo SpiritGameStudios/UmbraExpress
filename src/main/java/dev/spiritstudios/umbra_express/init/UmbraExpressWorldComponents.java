2d5e49d964de7801a37bcecb63189af7eedeb79d
SkyNotTheLimit <159592458+ekulxam@users.noreply.github.com>
2025-12-01 23:39:13 -0800

    Fix: crystal ball somehow wasn't being registered

--- src/main/java/dev/spiritstudios/umbra_express/init/UmbraExpressWorldComponents.java
+++ src/main/java/dev/spiritstudios/umbra_express/init/UmbraExpressWorldComponents.java
@@ -1,6 +1,7 @@
 package dev.spiritstudios.umbra_express.init;
 
 import dev.spiritstudios.umbra_express.cca.BroadcastWorldComponent;
+import dev.spiritstudios.umbra_express.cca.CrystalBallWorldComponent;
 import org.ladysnake.cca.api.v3.world.WorldComponentFactoryRegistry;
 import org.ladysnake.cca.api.v3.world.WorldComponentInitializer;
 
@@ -9,5 +10,6 @@ public class UmbraExpressWorldComponents implements WorldComponentInitializer {
     @Override
     public void registerWorldComponentFactories(WorldComponentFactoryRegistry registry) {
         registry.register(BroadcastWorldComponent.KEY, BroadcastWorldComponent::new);
+		registry.register(CrystalBallWorldComponent.KEY, CrystalBallWorldComponent::new);
     }
 }
