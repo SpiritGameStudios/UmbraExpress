f4741ed6e29679cc0d04d2f18a0c96d7fe5b9638
SkyNotTheLimit <159592458+ekulxam@users.noreply.github.com>
2025-12-04 00:09:39 -0800

    Fix: event style for TMMPlayerEvents too

--- src/main/java/dev/spiritstudios/umbra_express/event/TMMPlayerEvents.java
+++ src/main/java/dev/spiritstudios/umbra_express/event/TMMPlayerEvents.java
@@ -9,45 +9,48 @@ import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Identifier;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.Nullable;
 
-@SuppressWarnings({"CodeBlock2Expr", "unused"})
-public class TMMPlayerEvents {
+@ApiStatus.NonExtendable
+public interface TMMPlayerEvents {
 
-    public static final Event<Initializing> INITIALIZING = EventFactory.createArrayBacked(Initializing.class, callbacks -> {
-        return (serverWorld, serverPlayer, role, playing, game) -> {
-            for (Initializing callback : callbacks)
-                callback.onInitializing(serverWorld, serverPlayer, role, playing, game);
-        };
-    });
+    Event<Initializing> INITIALIZING = EventFactory.createArrayBacked(Initializing.class,
+		callbacks -> (serverWorld, serverPlayer, role, playing, game) -> {
+			for (Initializing callback : callbacks) {
+				callback.onInitializing(serverWorld, serverPlayer, role, playing, game);
+			}
+		}
+	);
 
-    public static final Event<Tick> TICK = EventFactory.createArrayBacked(Tick.class, callbacks -> {
-        return (serverWorld, serverPlayer, role, playing, game) -> {
-            for (Tick callback : callbacks)
-                callback.onTick(serverWorld, serverPlayer, role, playing, game);
-        };
-    });
+    Event<Tick> TICK = EventFactory.createArrayBacked(Tick.class,
+		callbacks -> (serverWorld, serverPlayer, role, playing, game) -> {
+			for (Tick callback : callbacks) {
+				callback.onTick(serverWorld, serverPlayer, role, playing, game);
+			}
+		}
+	);
 
-    public static final Event<Died> DIED = EventFactory.createArrayBacked(Died.class, callbacks -> {
-        return (world, player, playerRole, killer, killerRole, deathReason, game) -> {
-            for (Died callback : callbacks)
-                callback.onDied(world, player, playerRole, killer, killerRole, deathReason, game);
-        };
-    });
+    Event<Died> DIED = EventFactory.createArrayBacked(Died.class,
+		callbacks -> (world, player, playerRole, killer, killerRole, deathReason, game) -> {
+			for (Died callback : callbacks) {
+				callback.onDied(world, player, playerRole, killer, killerRole, deathReason, game);
+			}
+		}
+	);
 
     @FunctionalInterface
-    public interface Died {
+    interface Died {
         void onDied(World world, PlayerEntity player, Role playerRole, @Nullable PlayerEntity killer, @Nullable Role killerRole, Identifier deathReason, GameWorldComponent game);
     }
 
     @FunctionalInterface
-    public interface Initializing {
+    interface Initializing {
         void onInitializing(ServerWorld serverWorld, ServerPlayerEntity serverPlayer, Role role, boolean playing, GameWorldComponent game);
     }
 
     @FunctionalInterface
-    public interface Tick {
+    interface Tick {
         void onTick(ServerWorld serverWorld, ServerPlayerEntity serverPlayer, Role role, boolean playing, GameWorldComponent game);
     }
-
 }
