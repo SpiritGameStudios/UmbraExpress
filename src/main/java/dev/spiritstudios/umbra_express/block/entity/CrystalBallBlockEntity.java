278b718b4986e036bdc9aaea38a95759a382c0a4
SkyNotTheLimit <159592458+ekulxam@users.noreply.github.com>
2025-12-04 23:23:57 -0800

    Fix: force sync crystal ball

--- src/main/java/dev/spiritstudios/umbra_express/block/entity/CrystalBallBlockEntity.java
+++ src/main/java/dev/spiritstudios/umbra_express/block/entity/CrystalBallBlockEntity.java
@@ -14,6 +14,7 @@ import net.minecraft.network.listener.ClientPlayPacketListener;
 import net.minecraft.network.packet.Packet;
 import net.minecraft.network.packet.s2c.play.BlockEntityUpdateS2CPacket;
 import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
@@ -99,6 +100,10 @@ public class CrystalBallBlockEntity extends BlockEntity {
 			);
 
         this.markDirty();
+
+		if (world instanceof ServerWorld serverWorld) {
+			serverWorld.getChunkManager().markForUpdate(pos);
+		}
 
         sendApparitionMessage(apparition, mystic);
         playSound(world, pos, random);
