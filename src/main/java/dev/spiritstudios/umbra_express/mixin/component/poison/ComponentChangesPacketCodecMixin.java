20a2354957b275c58a3bd5e5e0454c3389128ce7
SkyNotTheLimit <159592458+ekulxam@users.noreply.github.com>
2025-12-18 01:02:38 -0800

    Feat: hide poison component from client

---
+++ src/main/java/dev/spiritstudios/umbra_express/mixin/component/poison/ComponentChangesPacketCodecMixin.java
@@ -0,0 +1,35 @@
+package dev.spiritstudios.umbra_express.mixin.component.poison;
+
+import dev.doctor4t.trainmurdermystery.index.TMMDataComponentTypes;
+import net.minecraft.component.ComponentChanges;
+import net.minecraft.component.ComponentType;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+
+@Mixin(targets = "net.minecraft.component.ComponentChanges$1")
+public class ComponentChangesPacketCodecMixin {
+
+	@ModifyVariable(method = "encode(Lnet/minecraft/network/RegistryByteBuf;Lnet/minecraft/component/ComponentChanges;)V", at = @At("HEAD"), index = 2, argsOnly = true)
+	private ComponentChanges removePoison(ComponentChanges original) {
+		ComponentChanges.Builder builder = ComponentChanges.builder();
+		for (Map.Entry<ComponentType<?>, Optional<?>> entry : original.entrySet()) {
+			ComponentType<?> type = entry.getKey();
+			if (Objects.equals(TMMDataComponentTypes.POISONER, type)) {
+				continue;
+			}
+			entry.getValue()
+				.ifPresentOrElse(value -> {
+					//noinspection unchecked, rawtypes
+					builder.add((ComponentType) type, value);
+				},
+				() -> builder.remove(type)
+			);
+		}
+		return builder.build();
+	}
+}
---
+++ src/main/java/dev/spiritstudios/umbra_express/mixin/component/poison/package-info.java
@@ -0,0 +1,3 @@
+package dev.spiritstudios.umbra_express.mixin.component.poison;
+
+// Attempts to fix https://github.com/doctor4t/TrainMurderMystery/issues/93
