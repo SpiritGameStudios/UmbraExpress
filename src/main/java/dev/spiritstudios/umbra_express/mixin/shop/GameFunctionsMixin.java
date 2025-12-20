63c0002d13e3b76760c65e3431161888dc25897a
SkyNotTheLimit <159592458+ekulxam@users.noreply.github.com>
2025-12-19 18:21:22 -0800

    Fix amountGainedPerKill applying to wrong player

--- src/main/java/dev/spiritstudios/umbra_express/mixin/shop/GameFunctionsMixin.java
+++ src/main/java/dev/spiritstudios/umbra_express/mixin/shop/GameFunctionsMixin.java
@@ -7,6 +7,8 @@ import dev.doctor4t.trainmurdermystery.game.GameConstants;
 import dev.doctor4t.trainmurdermystery.game.GameFunctions;
 import dev.spiritstudios.umbra_express.role.MoneyManager;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.Identifier;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
@@ -15,11 +17,11 @@ import org.spongepowered.asm.mixin.injection.ModifyConstant;
 public class GameFunctionsMixin {
 
     @ModifyConstant(method = "killPlayer(Lnet/minecraft/entity/player/PlayerEntity;ZLnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/util/Identifier;)V", constant = @Constant(intValue = GameConstants.MONEY_PER_KILL), remap = false)
-    private static int addToBalanceOnKill(int constant, @Local(argsOnly = true, name = "arg2") PlayerEntity killer) {
+    private static int addToBalanceOnKill(int constant, PlayerEntity victim, boolean spawnBody, PlayerEntity killer, Identifier deathReason) {
         GameWorldComponent game = GameWorldComponent.KEY.get(killer.getWorld());
         Role role = game.getRole(killer);
 
-        return MoneyManager.ROLE_MAP.containsKey(role) ? MoneyManager.ROLE_MAP.get(role).amountGainedPerKill().apply(killer) : 0;
+        return MoneyManager.ROLE_MAP.containsKey(role) ? MoneyManager.ROLE_MAP.get(role).amountGainedPerKill().apply(victim) : 0;
     }
 
 }
