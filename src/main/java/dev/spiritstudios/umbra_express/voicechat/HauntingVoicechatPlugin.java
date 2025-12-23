ae3028564c39fd214a23eeb8af4b2e96978e1d33
SkyNotTheLimit <159592458+ekulxam@users.noreply.github.com>
2025-12-22 20:03:31 -0800

    Revert "Fix: maybe entity audio channels"
    This reverts commit be851832fca01d894fa34a2790a4f266e38c06da.

--- src/main/java/dev/spiritstudios/umbra_express/voicechat/HauntingVoicechatPlugin.java
+++ src/main/java/dev/spiritstudios/umbra_express/voicechat/HauntingVoicechatPlugin.java
@@ -3,7 +3,6 @@ package dev.spiritstudios.umbra_express.voicechat;
 import de.maxhenkel.voicechat.api.Group;
 import de.maxhenkel.voicechat.api.VoicechatConnection;
 import de.maxhenkel.voicechat.api.VoicechatPlugin;
-import de.maxhenkel.voicechat.api.audiochannel.EntityAudioChannel;
 import de.maxhenkel.voicechat.api.audiochannel.StaticAudioChannel;
 import de.maxhenkel.voicechat.api.events.EventRegistration;
 import de.maxhenkel.voicechat.api.events.MicrophonePacketEvent;
@@ -12,12 +11,9 @@ import dev.doctor4t.trainmurdermystery.compat.TrainVoicePlugin;
 import dev.spiritstudios.umbra_express.UmbraExpress;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.Nullables;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 import java.util.UUID;
@@ -26,8 +22,10 @@ import static dev.doctor4t.trainmurdermystery.compat.TrainVoicePlugin.SERVER_API
 
 public class HauntingVoicechatPlugin implements VoicechatPlugin {
 	public static final Identifier ID = UmbraExpress.id("haunting");
-	public static final Map<UUID, EntityAudioChannel> HAUNT_CHANNELS = new HashMap<>();
-	public static final Set<ServerPlayerEntity> KILLERS = new HashSet<>();
+	public static final UUID HAUNT_UUID = UUID.randomUUID();
+	@Nullable
+	public static StaticAudioChannel hauntChannel;
+	public static final Set<ServerPlayerEntity> killers = new HashSet<>();
 
 	public static void initHauntForKiller(ServerPlayerEntity player) {
 		if (TrainVoicePlugin.isVoiceChatMissing()) {
@@ -39,7 +37,16 @@ public class HauntingVoicechatPlugin implements VoicechatPlugin {
 			return;
 		}
 
-		KILLERS.add(player);
+		if (hauntChannel == null) {
+			hauntChannel = SERVER_API.createStaticAudioChannel(HAUNT_UUID);
+		}
+
+		if (hauntChannel == null) {
+			return;
+		}
+
+		killers.add(player);
+		hauntChannel.addTarget(playerConnection);
 	}
 
 	@Override
@@ -57,7 +64,7 @@ public class HauntingVoicechatPlugin implements VoicechatPlugin {
 			return;
 		}
 
-		if (KILLERS.contains(serverPlayer)) {
+		if (killers.contains(serverPlayer)) {
 			return;
 		}
 
@@ -72,20 +79,12 @@ public class HauntingVoicechatPlugin implements VoicechatPlugin {
 			return;
 		}
 
-		UUID uuid = serverPlayer.getUuid();
-		if (!HAUNT_CHANNELS.containsKey(uuid) || HAUNT_CHANNELS.get(uuid) == null) {
-			HAUNT_CHANNELS.put(uuid, SERVER_API.createEntityAudioChannel(UUID.randomUUID(), SERVER_API.fromEntity(serverPlayer)));
-		}
-
-		EntityAudioChannel hauntChannel = HAUNT_CHANNELS.get(uuid);
-		if (hauntChannel != null) {
-			hauntChannel.send(microphonePacket);
-		}
+		if (hauntChannel != null) hauntChannel.send(microphonePacket);
 	}
 
 	public static void reset() {
-		HAUNT_CHANNELS.clear();
-		KILLERS.clear();
+		if (hauntChannel != null) hauntChannel.clearTargets();
+		killers.clear();
 	}
 
 	@Override
